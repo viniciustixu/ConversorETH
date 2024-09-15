21862c54227be385a1a58f3e567fce85dcf6fe90
Vinicius Souza <vinicipkt@live.com>
2024-09-15 07:22:39 -0300

    Melhorias

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+window.onload = function () {
+  const ethPriceStored = localStorage.getItem('valorAtualETH');
+  if (ethPriceStored) {
+    document.getElementById('valorAtualETH').value = ethPriceStored;
+  }
+
+  document.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      converter();
+    }
+  });
+
+};
+
 function converter() {
-  const ethPrice = 2466.66;
+  const valorAtualETH = parseFloat(document.getElementById('valorAtualETH').value);
   const valorDesejado = parseFloat(document.getElementById("valorDesejado").value);
   const resultado = document.getElementById("resultado");
 
-  const preçoEmEth = (valorDesejado - (valorDesejado * 0.05)) / ethPrice;
+  if (!valorAtualETH || !valorDesejado) {
+    alert("Por favor, insira os valores corretamente.");
+    return;
+  }
+
+
+  localStorage.setItem('valorAtualETH', valorAtualETH);
+
+  const preçoEmEth = (valorDesejado - (valorDesejado * 0.05)) / valorAtualETH;
 
   navigator.clipboard.writeText(preçoEmEth.toFixed(6));
   resultado.innerText = preçoEmEth.toFixed(6);
-
-};
+}
